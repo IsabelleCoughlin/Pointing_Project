727f4204ea36663e4c867eb2816aabdfb9b57095
IsabelleCoughlin <isabellecoughlin@gmail.com>
2025-07-17 12:25:37 -0400

    Creating rudimentary DFM socket code

--- create_df.py
+++ create_df.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-column_names = ['Object Name', 'Az (Rot)', 'El (Rot)', 'Az', 'El', 'Az Off (Rot)', 'El Off (Rot)']
+column_names = ['Time', 'Target Az', 'Target El', 'Rotor_Az', 'Rotor_El']
 
 empty_df = pd.DataFrame(columns = column_names)
-empty_df.to_csv('Pulsars.csv', index=False)
+empty_df.to_csv('DFM_Data.csv', index=False)
